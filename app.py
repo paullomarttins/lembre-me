b60d506fe7f476c3c749dcddd56ab46f83cc1fe2
Paulo Martins <p.o.marttins@gmail.com>
2024-10-08 23:43:56 -0300

    Ajuste no sqlite

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lembre_me_.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lembre_me.sqlite3'
 db = SQLAlchemy(app)
 
 class Tarefa(db.Model):
